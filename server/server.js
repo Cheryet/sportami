0a785de0181aeae2547a12a6fa49aa4ffc5fbd70
Cheryet <C.heryet@outlook.com>
2023-01-06 14:47:58 -0700

    add server functionality

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
 // Express Server
 const express = require("express");
+const bodyParser = require("body-parser");
+
+// Middleware
+const app = express();
+const port = process.env.PORT || 8001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Hello from server 👋🏼");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} 👋🏼`);
+});
