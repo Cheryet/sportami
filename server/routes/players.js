ef0cd4d4099b5a39fd7584fac8cab885f311a79d
Cheryet <C.heryet@outlook.com>
2023-01-06 16:25:08 -0700

    refactor routes to be more consise

--- server/routes/players.js
+++ server/routes/players.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+//Routes for api/players
+
 router.get("/", (req, res) => {
   //DB Query goes here
 });
 
-router.get("/:id", (req, res) => {
-  //DB Query goes here
-});
-
-router.put("/:id", (req, res) => {
-  //DB Query goes here
-});
-
-router.post("/", (req, res) => {
-  //DB Query goes here
-});
+router
+  .route("/:id")
+  .get("/:id", (req, res) => {
+    //DB Query goes here
+  })
+  .put("/:id", (req, res) => {
+    //DB Query goes here
+  })
+  .post("/id", (req, res) => {
+    //DB Query goes here
+  });
 
 module.exports = router;
