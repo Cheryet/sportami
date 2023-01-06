c8fd42cfec34b640296a5a47a88253aa5a1fcec3
Cheryet <C.heryet@outlook.com>
2023-01-06 16:26:38 -0700

    refactor to be more consise

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+//Routes for /api/reviews
+
 router.get("/", (res, req) => {
   //DB Query goes here
 });
 
-router.get("/:id", (res, req) => {
-  //DB Query goes here
-});
-
-router.post("/", (res, req) => {
-  //DB Query goes here
-});
+router
+  .route("/:id")
+  .get("/:id", (res, req) => {
+    //DB Query goes here
+  })
+  .post("/id", (res, req) => {
+    //DB Query goes here
+  });
 
 module.exports = router;
