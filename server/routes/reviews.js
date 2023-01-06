c7fbe9ccc908169af7eaff62500c67604e9034c5
Cheryet <C.heryet@outlook.com>
2023-01-06 16:11:04 -0700

    add routes for reviews

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (res, req) => {
+  //DB Query goes here
+});
+
+router.get("/:id", (res, req) => {
+  //DB Query goes here
+});
+
+router.post("/", (res, req) => {
+  //DB Query goes here
+});
+
+module.exports = router;
