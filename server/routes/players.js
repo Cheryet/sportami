0ce2d900448b4018958b285d56710704bee25f66
Cheryet <C.heryet@outlook.com>
2023-01-06 15:55:31 -0700

    inital commit

---
+++ server/routes/players.js
@@ -0,0 +1,15 @@
+app.get("/api/players", (req, res) => {
+  //DB Query goes here
+});
+
+app.get("/api/players/:id", (req, res) => {
+  //DB Query goes here
+});
+
+app.put("/api/players/:id", (req, res) => {
+  //DB Query goes here
+});
+
+app.post("/api/players", (req, res) => {
+  //DB Query goes here
+});
