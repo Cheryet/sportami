b5289debcf16f75aae02a94caadb1a81f3076cb7
Cheryet <C.heryet@outlook.com>
2023-01-06 16:06:06 -0700

    add routes for player sports

---
+++ server/routes/player_sports.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  //DB Query goes here
+});
+
+router.get("/:id", (req, res) => {
+  //DB Query goes here
+});
+
+router.post("/:id", (req, res) => {
+  //DB Query goes here
+});
+
+module.exports = router;
