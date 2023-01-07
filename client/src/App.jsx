f84a12ba3ce31242d5b099fb8b632e86499a1477
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-07 10:20:02 -0500

    Build and style Location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import "./app.scss";
 import Home from "./pages/home/Home";
 import Credentials from "./pages/register/credentials-1/Credentials";
 import EmailName from "./pages/register/emailName-2/EmailName";
+import Location from "./pages/register/location-3/Location";
 
 const App = () => {
-  return <Credentials />;
+  return <Location />;
 };
 
 export default App;
---
+++ client/src/pages/register/location-3/Location.jsx
@@ -0,0 +1,22 @@
+import "./location.scss";
+import { ArrowBackIos } from "@material-ui/icons";
+
+function Location() {
+  return (
+    <div className="location">
+      <ArrowBackIos />
+      <h4>My nearest location to play is</h4>
+      <select required name="cities" id="cities">
+        <option value="" selected>
+          Select option
+        </option>
+        <option value="one">City 1</option>
+        <option value="two">City 2</option>
+        <option value="three">City 3</option>
+      </select>
+      <button className="continue-button">Continue</button>
+    </div>
+  );
+}
+
+export default Location;
