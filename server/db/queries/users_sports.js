7d0ad2f0e8384af5b7388d2a32b2cd77474e37e6
Ben Stolb <benstolb@protonmail.com>
2023-01-06 19:13:11 -0800

    Add adduserSports query

--- server/db/queries/users_sports.js
+++ server/db/queries/users_sports.js
@@ -6,4 +6,19 @@ const getAllUserSports = () => {
 	})
 }
 
-module.exports = {getAllUserSports}
+const getUserSportsById = (id) => {
+  return db.query("SELECT * FROM users_sports WHERE id = $1", [id]).then((data) => {
+    return data.rows;
+  });
+};
+
+const addUserSports = (user_id, sport_id, self_skill) => {
+	return db.query(`
+		INSERT INTO user_sports (user_id, sport_id, self_skill) VALUES ($1, $2, $3, $4, $5)`, 
+    [user_id, sport_id, self_skill]).then(
+		data => {
+			return data.rows;
+	})
+}
+
+module.exports = {getAllUserSports, getUserSportsById, addUserSports}
