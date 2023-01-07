6eed882f700ba6d3d0dc595b7c2fad7aa4aaacf3
Cheryet <C.heryet@outlook.com>
2023-01-07 16:03:22 -0700

    add about me and skills to component

--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./profile.scss";
 import * as TbIcon from "react-icons/tb";
+import * as MdIcon from "react-icons/md";
 
 const Profile = () => {
   return (
@@ -20,9 +21,15 @@ const Profile = () => {
             />
           </div>
           <div className="user-info">
-            <p className="name">Corbin Heryet</p>
-            <p className="username">@Cheryet</p>
-            <p className="email">C.Heryet@outlook.com</p>
+            <p className="name-title">NAME</p>
+            <hr className="name-line" />
+            <p className="item name">Corbin Heryet</p>
+            <p className="username-title">USERNAME</p>
+            <hr className="username-line" />
+            <p className="item username">@Cheryet</p>
+            <p className="email-title">EMAIL</p>
+            <hr className="email-line" />
+            <p className="item email">C.Heryet@outlook.com</p>
           </div>
         </div>
         <div className="middle-container">
@@ -30,12 +37,38 @@ const Profile = () => {
             <div className="location-container">
               <TbIcon.TbMapPin className="pin-icon" />
               <p>Lethbridge</p>
+              <MdIcon.MdOutlineKeyboardArrowDown className="down-icon" />
             </div>
-            <p>Male</p>
+            <p className="gender">Male</p>
           </div>
-          <div className="bio">
-            <p>This is where the bio state will go eventually</p>
+          <div className="bio-container">
+            <p className="about-me">ABOUT ME</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
           </div>
+        </div>
+        <div className="bottom-container">
+          <div className="sports-container">
+            <p className="sports-title">SPORTS</p>
+            <ul className="sports-list">
+              <li className="sports-item">Tennis</li>
+              <li className="sports-item">Golf</li>
+              <li className="sports-item">Ping Pong</li>
+            </ul>
+          </div>
+          <div className="skill-level-container">
+            <p className="skill-level-title">SKILL LEVEL</p>
+            <ul className="skill-level-list">
+              <li className="skill-level-item">Casual</li>
+              <li className="skill-level-item">Casual</li>
+              <li className="skill-level-item">Casual</li>
+            </ul>
+          </div>
+        </div>
+        <div className="button-container">
+          <button>EDIT PROFILE</button>
         </div>
       </div>
     </>
