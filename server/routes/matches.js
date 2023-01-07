501a0dab151a7ce36c8e7d4b418a2154bd2c652a
Cheryet <C.heryet@outlook.com>
2023-01-06 20:34:06 -0700

    add comments

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -6,6 +6,7 @@ const db = require("../db/queries/matches_queries");
 // ~~ Routes for /api/matches ~~
 //
 
+//This route gets all matches
 router.get("/", (req, res) => {
   db.getAllMatches().then((data) => {
     const matches = data;
@@ -15,17 +16,20 @@ router.get("/", (req, res) => {
 
 router
   .route("/:id")
+  //This route gets a single match by ID
   .get((req, res) => {
     db.getMatchById(req.params.id).then((data) => {
       const match = data;
       res.send(match);
     });
   })
+  //This route updates an existing match with the opponent
   .put((req, res) => {
     db.setOpponent(req.body.opponent_id, req.body.match_id).then(() => {
       res.send(204).json();
     });
   })
+  //This route creates a match
   .post((req, res) => {
     db.addMatch(
       req.body.challenger_id,
