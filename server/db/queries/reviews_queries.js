df6496e79d74b3f64345541048547e8ae6e25fe2
Cheryet <C.heryet@outlook.com>
2023-01-06 19:40:24 -0700

    add queries for reviews table

--- server/db/queries/reviews_queries.js
+++ server/db/queries/reviews_queries.js
@@ -6,3 +6,30 @@ const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME || "sportami",
 });
+
+const getAllReviews = () => {
+  return pool
+    .query(`SELECT * FROM reviews`)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+const getReviewsById = (id) => {
+  return pool
+    .query(`SELECT * FROM reviews WHERE id = $1`, [id])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+module.exports = {
+  getAllReviews,
+  getReviewsById,
+};
