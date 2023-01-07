77193ceadf93928792871c9ab5483bbdb6a1ddf8
Cheryet <C.heryet@outlook.com>
2023-01-06 19:38:31 -0700

    add queries for matches

--- server/db/queries/matches_queries.js
+++ server/db/queries/matches_queries.js
@@ -6,3 +6,30 @@ const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME || "sportami",
 });
+
+const getAllMatches = () => {
+  return pool
+    .query(`SELECT * FROM matches`)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+const getMatchById = (id) => {
+  return pool
+    .query(`SELECT * FROM matches WHERE id = $1`, [id])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+module.exports = {
+  getAllMatches,
+  getMatchById,
+};
