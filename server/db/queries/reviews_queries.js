4dd870475cf278b0c13537d71e45854647390f3e
Cheryet <C.heryet@outlook.com>
2023-01-06 19:55:23 -0700

    add queries for adding review and setting winner

--- server/db/queries/reviews_queries.js
+++ server/db/queries/reviews_queries.js
@@ -7,6 +7,8 @@ const pool = new Pool({
   database: process.env.DB_NAME || "sportami",
 });
 
+// Queries for Reviews Table
+
 const getAllReviews = () => {
   return pool
     .query(`SELECT * FROM reviews`)
@@ -29,7 +31,20 @@ const getReviewsById = (id) => {
     });
 };
 
+const addReview = (user_sport_id, winner_id, sportsmanship_rating) => {
+  return pool.query(
+    `INSERT INTO reviews (user_sport_id, winner_id, sportsmanship_rating) VALUES ($1, $2, $3)`,
+    [user_sport_id, winner_id, sportsmanship_rating]
+  );
+};
+
+const setWinner = (winner_id, review_id) => {
+  return pool.query(`UPDATE reviews SET $1 WHERE = $2`, [winner_id, review_id]);
+};
+
 module.exports = {
   getAllReviews,
   getReviewsById,
+  addReview,
+  setWinner,
 };
