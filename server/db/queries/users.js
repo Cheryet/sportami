105d3dd921fec27bf9ca90af8fa82d99bb5e1029
Ben Stolb <benstolb@protonmail.com>
2023-01-06 18:58:17 -0800

    Add addUser query

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -12,5 +12,11 @@ const getUserById = (id) => {
   });
 };
 
+const addUser = (username, password, email, bio, location, gender, age, profile_pic) => {
+  return db.query(
+    "INSERT INTO users (username, password, email, bio, location, gender, age, profile_pic) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;",
+    [username, password, email, bio, location, gender, age, profile_pic]
+  );
+};
 
-module.exports = {getAllUsers, getUserById}
+module.exports = {getAllUsers, getUserById, addUser}
