c4b8c688bd76580a1c5ab0cb604afe51f2fa477d
Ben Stolb <benstolb@protonmail.com>
2023-01-06 20:26:07 -0800

    Add new sports query

--- server/db/queries/sports.js
+++ server/db/queries/sports.js
@@ -6,4 +6,10 @@ const getAllSports = () => {
 	})
 }
 
-module.exports = {getAllSports}
+const getSportById = (id) => {
+  return db.query("SELECT * FROM sports WHERE id = $1", [id]).then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = {getAllSports, getSportById}
