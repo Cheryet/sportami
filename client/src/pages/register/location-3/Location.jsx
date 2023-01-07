10b77cc6af854ddddfa4b7974efd3744e4127240
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-07 10:25:20 -0500

    Style Location

--- client/src/pages/register/location-3/Location.jsx
+++ client/src/pages/register/location-3/Location.jsx
@@ -7,8 +7,8 @@ function Location() {
       <ArrowBackIos />
       <h4>My nearest location to play is</h4>
       <select required name="cities" id="cities">
-        <option value="" selected>
-          Select option
+        <option value="" selected disabled>
+          Select an option
         </option>
         <option value="one">City 1</option>
         <option value="two">City 2</option>
