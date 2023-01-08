96835548dc2576c9693e56ca9afccfac4cf73b7d
Cheryet <C.heryet@outlook.com>
2023-01-08 11:13:32 -0700

    update imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,8 @@
 import "./app.scss";
-
 import Home from "./pages/home/Home";
-import Credentials from "./pages/register/credentials-1/Credentials";
-import EmailName from "./pages/register/emailName-2/EmailName";
-import Location from "./pages/register/location-3/Location";
-import BirthGender from "./pages/register/birthGender-4/BirthGender";
-import ProfilePhoto from "./pages/register/profilePhoto-5/ProfilePhoto";
-import Bio from "./pages/register/bio-6/Bio";
+import Main from "./pages/main/Main";
 
-import userAppData from "./hooks/userAppData";
 import { BrowserRouter, Route } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/register",
-    element: <Credentials />,
-  },
-  {
-    path: "/register/2",
-    element: <EmailName />,
-  },
-  {
-    path: "/register/3",
-    element: <Location />,
-  },
-]);
 
 const App = () => {
   return (
