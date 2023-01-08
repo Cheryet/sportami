e89a8d9f5a31d3b236ef973e72588ebc5a2d0697
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-01-08 05:07:08 +0000

    Add login state to Home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,21 +1,47 @@
+import React from "react";
 import "./home.scss";
+import { useState } from "react";
+import axios from "axios";
 
 function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async (event) => {
+    event.preventDefault();
+    try {
+      const loginSuccess = await axios.post("/api/login", {
+        email: email,
+        password: password,
+      });
+      console.log(loginSuccess);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // 123@gmail.com
+    // 12345
+  };
+
   return (
     <div className="background">
       <div className="home">
         <div className="logo">S P O R T A M I</div>
         <div className="slogan">Turn an ami into an enemy.</div>
-        <form className="login">
+        <form className="login" onSubmit={login}>
           <input
             className="username"
-            type="username"
+            type="text"
             placeholder="Username or email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="password"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           ></input>
           <button className="login-button">Login</button>
           <span>
