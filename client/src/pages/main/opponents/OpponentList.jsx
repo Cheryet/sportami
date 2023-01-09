dcbe4498c5f093e803dd2ac6d958cb4acef82d88
Cheryet <C.heryet@outlook.com>
2023-01-09 14:31:41 -0700

    add filter bar and opponent component

--- client/src/pages/main/opponents/OpponentList.jsx
+++ client/src/pages/main/opponents/OpponentList.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Opponents from "./Opponent";
 import "./opponentList.scss";
 
 const OpponentList = () => {
-  return <></>;
+  return (
+    <>
+      <div className="opponent-list">
+        <section className="description-container">
+          <p className="text">Start searching for opponents in your area!</p>
+          <button className="filter-button">FILTER</button>
+        </section>
+        <hr />
+        <Opponents />
+      </div>
+    </>
+  );
 };
 
 export default OpponentList;
