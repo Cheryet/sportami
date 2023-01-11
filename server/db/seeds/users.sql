9d4c0c376f80c3c9a288a7ef2ea3b44ab4888067
Cheryet <C.heryet@outlook.com>
2023-01-10 21:09:05 -0700

    add to seeding for testing purposes

--- server/db/seeds/users.sql
+++ server/db/seeds/users.sql
@@ -41,3 +41,25 @@ INSERT INTO users (
     19,
     'https://cdn.allthings.how/wp-content/uploads/2020/10/allthings.how-how-to-change-your-profile-picture-on-google-meet-profile-photo-759x427.png?width=800'
     );
+
+INSERT INTO users ( 
+  username,
+  first_name,
+  password,
+  email,
+  bio,
+  location,
+  gender,
+  age,
+  profile_pic) 
+  VALUES (
+    'Cheryet',
+    'Corbin',
+    '12345',
+    'example@gmail.com',
+    'This is my Bio. I need to write some text in here so im stretching it out, Hello from my bio',
+    'Lethrbidge',
+    'Male',
+    24,
+    'https://cdn.allthings.how/wp-content/uploads/2020/10/allthings.how-how-to-change-your-profile-picture-on-google-meet-profile-photo-759x427.png?width=800'
+    );
