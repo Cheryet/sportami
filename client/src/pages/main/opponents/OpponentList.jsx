22f69998e2957843845d844239ad23660d4db352
Cheryet <C.heryet@outlook.com>
2023-01-11 15:19:43 -0700

    add helper funtion to get opponents from db and pass to opponent component

--- client/src/pages/main/opponents/OpponentList.jsx
+++ client/src/pages/main/opponents/OpponentList.jsx
@@ -1,8 +1,53 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Opponents from "./Opponent";
 import "./opponentList.scss";
 
-const OpponentList = () => {
+const OpponentList = (props) => {
+  const [opponentData, setOpponentData] = useState({
+    opponents: [],
+    sports: [],
+  });
+
+  //Helper - Get all opponents
+  useEffect(() => {
+    const opponentsPromise = axios.get("/api/users");
+    const sportsPromise = axios.get("/api/user_sports");
+
+    Promise.all([opponentsPromise, sportsPromise]).then((all) => {
+      setOpponentData({ opponents: all[0].data, sports: all[1].data });
+    });
+  }, []);
+
+  //Helper - Pass down opponentData as props to component
+  //            if user is logged in, remove from list
+  let opponentList = [];
+  const getOpponentList = (token) => {
+    if (opponentData.opponents) {
+      opponentList = opponentData.opponents.map((item, index) => {
+        if (item.id != token) {
+          return (
+            <Opponents
+              key={index}
+              user_id={item.id}
+              age={item.age}
+              bio={item.bio}
+              location={item.location}
+              profile_pic={item.profile_pic}
+              username={item.username}
+              first_name={item.first_name}
+              sports={opponentData.sports}
+            />
+          );
+        }
+      });
+    }
+  };
+
+  getOpponentList(props.token);
+
   return (
     <>
       <div className="opponent-list">
@@ -11,7 +56,7 @@ const OpponentList = () => {
           <button className="filter-button">FILTER</button>
         </section>
         <hr />
-        <Opponents />
+        {opponentList}
       </div>
     </>
   );
