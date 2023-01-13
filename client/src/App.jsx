f715c44d9e9255d0891e0e2c42824a4a123c1c2b
Ben Stolb <benstolb@protonmail.com>
2023-01-13 00:30:24 -0800

    Fix app routes when logged in or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Accordion from "./components/filter/Accordion";
 import useToken from "./hooks/useToken";
 import ModeProvider from "./providers/ModeProvider";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
   const { token, setToken } = useToken();
@@ -18,7 +18,15 @@ const App = () => {
         <Routes>
           <Route
             path="/"
-            element={!token ? <Home setToken={setToken} /> : <Main />}
+            element={
+              !token ? (
+                <ModeProvider>
+                  <Home setToken={setToken} />
+                </ModeProvider>
+              ) : (
+                <Navigate to="/main" />
+              )
+            }
           />
           <Route
             path="/register"
@@ -28,9 +36,7 @@ const App = () => {
             path="/main"
             element={
               !token ? (
-                <ModeProvider>
-                  <Home setToken={setToken} />
-                </ModeProvider>
+                <Navigate to="/" />
               ) : (
                 <ModeProvider>
                   <Main token={token} />
