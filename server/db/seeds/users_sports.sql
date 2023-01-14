4416e402d3c152a1a2b31a5b585422e65c934f9a
Cheryet <C.heryet@outlook.com>
2023-01-14 12:24:55 -0700

    fix spelling errors in seeds

--- server/db/seeds/users_sports.sql
+++ server/db/seeds/users_sports.sql
@@ -1,7 +1,7 @@
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (1, 1, 'Casual');
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (1, 2, 'Pro');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (1, 1, 'Beginner');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (1, 2, 'Advanced');
 INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (2, 1, 'Intermediate');
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (2, 2, 'Casual');
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 2, 'Casual');
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 1, 'Amatuer');
-INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 4, 'Casual');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (2, 2, 'Beginner');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 2, 'Beginner');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 1, 'Intermediate');
+INSERT INTO users_sports ( user_id, sport_id, self_skill) VALUES (3, 4, 'Beginner');
